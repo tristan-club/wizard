28a82f400666935ab90f627d7bd49d4ed8d5de0e
mgintoki <904448980@qq.com>
2022-12-13 18:50:56 +0800

    Remove delete envelope msg code when envelope sold out

--- handler/discordhandler/handler/cmd_envelope/open_envelope.go
+++ handler/discordhandler/handler/cmd_envelope/open_envelope.go
@@ -98,18 +98,6 @@ func openEnvelopeHandler(ctx *dcontext.Context) error {
 				return he.NewServerError(pconst.CodeBotSendMsgError, "", err)
 			}
 
-			if openEnvelopeResp.CommonResponse.Code == pconst.CODE_ENVELOPE_SOLD_OUT {
-				msgId, err := tstore.PBGetStr(fmt.Sprintf("%s%s", pconst.EnvelopeStorePrefix, envelopeNo), pconst.EnvelopeStorePathMsgId)
-				if err != nil {
-					log.Error().Fields(map[string]interface{}{"action": "get envelope msg error", "error": err.Error(), "id": envelopeNo}).Send()
-					return nil
-				}
-				err = ctx.Session.ChannelMessageDelete(ctx.GetGroupChannelId(), msgId)
-				if err != nil {
-					log.Error().Fields(map[string]interface{}{"action": "delete red envelope error", "error": err.Error(), "id": envelopeNo}).Send()
-				}
-			}
-
 			return nil
 		} else if openEnvelopeResp.CommonResponse.Code == pconst.CODE_ENVELOP_STATE_INVALID {
 			// todo
@@ -205,15 +193,21 @@ func openEnvelopeHandler(ctx *dcontext.Context) error {
 				)
 			}
 
+			e := &discordgo.MessageEmbed{
+				Type:        "rich",
+				Title:       title,
+				Description: envelopeDetail,
+			}
+
+			if param.Photo != "" {
+				e.Image = &discordgo.MessageEmbedImage{URL: param.Photo}
+			}
+
 			me := &discordgo.MessageEdit{
 				Channel: channelId,
 				ID:      envelopeMsgId,
 				Embeds: []*discordgo.MessageEmbed{
-					{
-						Type:        "rich",
-						Title:       title,
-						Description: envelopeDetail,
-					},
+					e,
 				},
 			}
 
