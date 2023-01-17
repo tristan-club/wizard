7c02079d5c527dfe57b6077baf00a3d8a322ca7d
mgintoki <904448980@qq.com>
2023-01-17 10:11:25 +0800

    Remove channel rate limiter

--- handler/discordhandler/rate/limiter.go
+++ handler/discordhandler/rate/limiter.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/tristan-club/wizard/handler/msglimiter"
 	"golang.org/x/time/rate"
-	"strconv"
 )
 
 var idLimiter *msglimiter.Limiter
@@ -12,16 +11,16 @@ var globalLimiter *rate.Limiter
 
 // NewLimiter creates both chat and global rate limiters.
 func init() {
-	idLimiter = msglimiter.NewIdRateLimiter(rate.Limit(1), 20)
-	globalLimiter = rate.NewLimiter(rate.Limit(50), 50)
+	//idLimiter = msglimiter.NewIdRateLimiter(rate.Limit(10), 20)
+	globalLimiter = rate.NewLimiter(rate.Limit(40), 40)
 }
 
 func CheckLimit(chatId string) {
 	globalLimiter.Wait(context.Background())
-	if chatId != "" {
-		cid, err := strconv.ParseInt(chatId, 10, 64)
-		if err == nil {
-			idLimiter.GetLimiter(cid).Wait(context.Background())
-		}
-	}
+	//if chatId != "" {
+	//	cid, err := strconv.ParseInt(chatId, 10, 64)
+	//	if err == nil {
+	//		idLimiter.GetLimiter(cid).Wait(context.Background())
+	//	}
+	//}
 }
